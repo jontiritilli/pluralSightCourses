97e3558e89099183a35700c61427bb25f923140a
Jonathan Tiritilli <jonathantiritilli@gmail.com>
2018-12-10 13:53:11 -0800

    change form to use Reactive methods vs template

--- Angular-ReactiveForms/Demo-Start/src/app/customers/customer.component.ts
+++ Angular-ReactiveForms/Demo-Start/src/app/customers/customer.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { NgForm } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 import { Customer } from './customer';
 
@@ -9,15 +9,31 @@ import { Customer } from './customer';
   styleUrls: ['./customer.component.css']
 })
 export class CustomerComponent implements OnInit {
+  customerForm: FormGroup;
   customer = new Customer();
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.customerForm = new FormGroup({
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      email: new FormControl(),
+      sendCatalog: new FormControl(true),
+    });
   }
 
-  save(customerForm: NgForm) {
-    console.log(customerForm.form);
-    console.log('Saved: ' + JSON.stringify(customerForm.value));
+  populateTestData(): void {
+    this.customerForm.setValue({
+      firstName: 'John',
+      lastName: 'Kennedy',
+      email: 'testing@test.com',
+      sendCatalog: false,
+    })
+  }
+
+  save() {
+    console.log(this.customerForm);
+    console.log('Saved: ' + JSON.stringify(this.customerForm.value));
   }
 }
